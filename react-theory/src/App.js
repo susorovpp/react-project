4d21bc7785d0420a786a13b129a3dd6b5e105688
susorovpp <susorovpp@gmail.com>
2021-04-16 22:32:29 +0300

    add context API

--- react-theory/src/App.js
+++ react-theory/src/App.js
@@ -4,12 +4,15 @@ import Car from './Car/Car'
 import ErrorBoundary from './ErrorBoundary/ErrorBoundary'
 import Counter from './Counter/Counter'
 
+export const ClickedContext = React.createContext(false)
+
 class App extends Component {
 
   constructor(props) {
     super(props)
 
     this.state = {
+      clicked: false,
       cars: [
         {name: 'Ford', year: 2018},
         {name: 'Audi', year: 2016},
@@ -70,12 +73,17 @@ class App extends Component {
           {/* <h1>{this.state.pageTitle}</h1> */}
           <h1>{this.props.title}</h1>
 
-          <Counter />
+          <ClickedContext.Provider value={this.state.clicked}>
+            <Counter />
+          </ClickedContext.Provider>
+          
           <hr/>
           <button
             onClick={this.toggleCarsHandler}
             style={{marginTop: '20px'}}
           >Toggle cars</button>
+
+          <button onClick={() => this.setState({clicked: true})}>Change clicked</button>
 
           <div style={{
             width: 400,
--- react-theory/src/Counter/Counter.js
+++ react-theory/src/Counter/Counter.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react'
 import Auxiliary from '../hoc/Auxilliary'
+import Counter2 from '../Counter2/Counter2'
 
 
 export default class Counter extends Component {
@@ -8,10 +9,6 @@ export default class Counter extends Component {
     }
 
     addCounter = () => {
-        // this.setState({
-        //     counter: this.state.counter + 1
-        // })
-
         this.setState((prevState) => {
             return {
                 counter: prevState.counter + 1
@@ -23,15 +20,10 @@ export default class Counter extends Component {
         return (
             <Auxiliary>
                 <h2>Counter {this.state.counter}</h2>
+                <Counter2 />
                 <button onClick={this.addCounter}>+</button>
                 <button onClick={() => this.setState({counter: this.state.counter - 1})}>-</button>
             </Auxiliary>
         )
-
-        // return [
-        //     <h2 key={'1'}>Counter {this.state.counter}</h2>,
-        //     <button key={'2'} onClick={this.addCounter}>+</button>,
-        //     <button key={'3'} onClick={() => this.setState({counter: this.state.counter - 1})}>-</button>
-        // ]
     }
 }
