d791dffd5a8cf090132f4c9d35d63d3a438b5178
susorovpp <susorovpp@gmail.com>
2021-04-17 21:24:36 +0300

    add change question

--- react-quiz/src/Containers/Quiz/Quiz.js
+++ react-quiz/src/Containers/Quiz/Quiz.js
@@ -4,15 +4,28 @@ import ActiveQuiz from '../../Components/ActiveQuiz/ActiveQuiz'
 
 class Quiz extends Component {
     state = {
+        activeQuestion: 0,
         quiz: [
             {
                 question: 'Какого цвета небо?',
                 rightAnswerId: 2,
+                id: 1,
                 answers: [
                     {text: 'Черный', id: 1},
                     {text: 'Синий', id: 2},
                     {text: 'Красный', id: 3},
                     {text: 'Зеленый', id: 4},
+                ]
+            },
+            {
+                question: 'В каком году основали Москву?',
+                rightAnswerId: 1,
+                id: 2,
+                answers: [
+                    {text: '1147', id: 1},
+                    {text: '1201', id: 2},
+                    {text: '1154', id: 3},
+                    {text: '1063', id: 4},
                 ]
             }
         ]
@@ -20,6 +33,10 @@ class Quiz extends Component {
 
     onAnswerClickHandler = (answerId) => {
         console.log('Answer Id: ', answerId);
+        
+        this.setState({
+            activeQuestion: this.state.activeQuestion + 1
+        })
     }
 
     render() {
@@ -29,9 +46,11 @@ class Quiz extends Component {
                 <div className={classes.QuizWrapper}>
                     <h1>Ответьте на все вопросы</h1>
                     <ActiveQuiz 
-                        answers={this.state.quiz[0].answers}
-                        question={this.state.quiz[0].question}
+                        answers={this.state.quiz[this.state.activeQuestion].answers}
+                        question={this.state.quiz[this.state.activeQuestion].question}
                         onAnswerClick={this.onAnswerClickHandler}
+                        quizLength={this.state.quiz.length}
+                        answerNumber={this.state.activeQuestion + 1}
                     />
                 </div>
             </div>
