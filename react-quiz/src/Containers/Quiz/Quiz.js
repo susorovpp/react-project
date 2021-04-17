1f9723dcb5a37e99fe9fc77ddce4c85643663d05
susorovpp <susorovpp@gmail.com>
2021-04-17 16:47:53 +0300

    add AnswerItem, AnswersList

--- react-quiz/src/Containers/Quiz/Quiz.js
+++ react-quiz/src/Containers/Quiz/Quiz.js
@@ -1,15 +1,31 @@
 import React, {Component} from 'react'
 import classes from './Quiz.module.css'
+import ActiveQuiz from '../../Components/ActiveQuiz/ActiveQuiz'
 
 class Quiz extends Component {
     state = {
-        quiz: []
+        quiz: [
+            {
+                answers: [
+                    {text: 'Вопрос 1'},
+                    {text: 'Вопрос 2'},
+                    {text: 'Вопрос 3'},
+                    {text: 'Вопрос 4'},
+                ]
+            }
+        ]
     }
 
     render() {
         return (
             <div className={classes.Quiz}>
-                <h1>Quiz</h1>
+
+                <div className={classes.QuizWrapper}>
+                    <h1>Ответьте на все вопросы</h1>
+                    <ActiveQuiz 
+                        answers={this.state.quiz[0].answers}
+                    />
+                </div>
             </div>
         )
     }
