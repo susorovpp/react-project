e527afa17bec954af4b7915e93f5bd773fec445e
susorovpp <susorovpp@gmail.com>
2021-04-18 11:56:29 +0300

    add button toggle menu

--- react-quiz/src/hoc/Layout/Layout.js
+++ react-quiz/src/hoc/Layout/Layout.js
@@ -1,11 +1,27 @@
 import React, {Component} from 'react'
 import classes from './Layout.module.css'
+import MenuToggle from '../../Components/Navigation/MenuToggle/MenuToggle'
 
 class Layout extends Component {
+
+    state = {
+        menu: false
+    }
+
+    toggleMenuHandler = () => {
+        this.setState({
+            menu: !this.state.menu
+        })
+    }
+
+
     render() {
         return (
             <div className={classes.Layout}>
-
+                <MenuToggle
+                    onToggle={this.toggleMenuHandler}
+                    isOpen={this.state.menu}
+                />
 
                 <main>
                     {this.props.children}
