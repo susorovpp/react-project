fe9cf03cc0049dc17b4540481e9507e376b9ee14
ivan.ivanov <vanya_ivanivanov_2021_ivanov@mail.ru>
2021-04-27 14:42:01 +0300

    add Auth form

--- react-quiz/src/Containers/Quiz/Quiz.js
+++ react-quiz/src/Containers/Quiz/Quiz.js
@@ -1,126 +1,128 @@
-import React, {Component} from 'react'
-import classes from './Quiz.module.css'
-import ActiveQuiz from '../../Components/ActiveQuiz/ActiveQuiz'
-import FinishedQuiz from '../../Components/FinishedQuiz/FinishedQuiz'
+import React, { Component } from "react";
+import classes from "./Quiz.module.css";
+import ActiveQuiz from "../../Components/ActiveQuiz/ActiveQuiz";
+import FinishedQuiz from "../../Components/FinishedQuiz/FinishedQuiz";
 
 class Quiz extends Component {
-    state = {
-        results: {}, // {[id]: 'success' || 'error' }
-        isFinished: false,
-        activeQuestion: 0,
-        answerState: null, // {[id]: 'success' || 'error'}
-        quiz: [
-            {
-                question: 'Какого цвета небо?',
-                rightAnswerId: 2,
-                id: 1,
-                answers: [
-                    {text: 'Черный', id: 1},
-                    {text: 'Синий', id: 2},
-                    {text: 'Красный', id: 3},
-                    {text: 'Зеленый', id: 4},
-                ]
-            },
-            {
-                question: 'В каком году основали Москву?',
-                rightAnswerId: 1,
-                id: 2,
-                answers: [
-                    {text: '1147', id: 1},
-                    {text: '1201', id: 2},
-                    {text: '1154', id: 3},
-                    {text: '1063', id: 4},
-                ]
-            }
-        ]
+  state = {
+    results: {}, // {[id]: 'success' || 'error' }
+    isFinished: false,
+    activeQuestion: 0,
+    answerState: null, // {[id]: 'success' || 'error'}
+    quiz: [
+      {
+        question: "Какого цвета небо?",
+        rightAnswerId: 2,
+        id: 1,
+        answers: [
+          { text: "Черный", id: 1 },
+          { text: "Синий", id: 2 },
+          { text: "Красный", id: 3 },
+          { text: "Зеленый", id: 4 },
+        ],
+      },
+      {
+        question: "В каком году основали Москву?",
+        rightAnswerId: 1,
+        id: 2,
+        answers: [
+          { text: "1147", id: 1 },
+          { text: "1201", id: 2 },
+          { text: "1154", id: 3 },
+          { text: "1063", id: 4 },
+        ],
+      },
+    ],
+  };
+
+  onAnswerClickHandler = (answerId) => {
+    if (this.state.answerState) {
+      const key = Object.keys(this.state.answerState)[0];
+      if (this.state.answerState[key] === "success") {
+        return;
+      }
     }
 
-    onAnswerClickHandler = (answerId) => {
-        if (this.state.answerState) {
-            const key = Object.keys(this.state.answerState)[0]
-            if (this.state.answerState[key] === 'success') {
-                return
-            }
-        }
+    console.log("Answer Id: ", answerId);
 
-        console.log('Answer Id: ', answerId);
+    const question = this.state.quiz[this.state.activeQuestion];
+    const results = this.state.results;
 
-        const question = this.state.quiz[this.state.activeQuestion]
-        const results = this.state.results
+    if (question.rightAnswerId === answerId) {
+      if (!results[question.id]) {
+        results[question.id] = "success";
+      }
+      this.setState({
+        answerState: { [answerId]: "success" },
+        results,
+      });
 
-        if (question.rightAnswerId === answerId) {
-            if (!results[question.id]) {
-                results[question.id] = 'success'
-            }
-            this.setState({
-                answerState: {[answerId]: 'success'},
-                results
-            })
-
-            const timeout = window.setTimeout(() => {
-                if (this.isQuizFinished()) {
-                    this.setState({
-                        isFinished: true
-                    })
-                } else {
-                    this.setState({
-                        activeQuestion: this.state.activeQuestion + 1,
-                        answerState: null
-                    })
-                }
-
-                window.clearTimeout(timeout)
-            }, 1000)
+      const timeout = window.setTimeout(() => {
+        if (this.isQuizFinished()) {
+          this.setState({
+            isFinished: true,
+          });
         } else {
-            results[question.id] = 'error'
-            this.setState({
-                answerState: {[answerId]: 'error'},
-                results
-            })
-        }
-    }
-
-    isQuizFinished() {
-        return this.state.activeQuestion + 1 === this.state.quiz.length
-    }
-
-    retryHandler = () => {
-        this.setState({
-            activeQuestion: 0,
+          this.setState({
+            activeQuestion: this.state.activeQuestion + 1,
             answerState: null,
-            isFinished:false,
-            results: {}
-        })
+          });
+        }
+
+        window.clearTimeout(timeout);
+      }, 1000);
+    } else {
+      results[question.id] = "error";
+      this.setState({
+        answerState: { [answerId]: "error" },
+        results,
+      });
     }
+  };
 
-    render() {
-        return (
-            <div className={classes.Quiz}>
+  isQuizFinished() {
+    return this.state.activeQuestion + 1 === this.state.quiz.length;
+  }
 
-                <div className={classes.QuizWrapper}>
-                    <h1>Ответьте на все вопросы</h1>
+  retryHandler = () => {
+    this.setState({
+      activeQuestion: 0,
+      answerState: null,
+      isFinished: false,
+      results: {},
+    });
+  };
 
-                    {
-                        this.state.isFinished
-                            ? <FinishedQuiz
-                                results={this.state.results}
-                                quiz={this.state.quiz}
-                                onRetry={this.retryHandler}
-                                />
-                            : <ActiveQuiz 
-                                answers={this.state.quiz[this.state.activeQuestion].answers}
-                                question={this.state.quiz[this.state.activeQuestion].question}
-                                onAnswerClick={this.onAnswerClickHandler}
-                                quizLength={this.state.quiz.length}
-                                answerNumber={this.state.activeQuestion + 1}
-                                state={this.state.answerState}
-                                />
-                    }
-                    
-                </div>
-            </div>
-        )
-    }
+	componentDidMount() {
+		console.log('Quiz ID = ', this.props.match.params.id);
+	}
+
+  render() {
+    return (
+      <div className={classes.Quiz}>
+        <div className={classes.QuizWrapper}>
+          <h1>Ответьте на все вопросы</h1>
+
+          {this.state.isFinished ? (
+            <FinishedQuiz
+              results={this.state.results}
+              quiz={this.state.quiz}
+              onRetry={this.retryHandler}
+            />
+          ) : (
+            <ActiveQuiz
+              answers={this.state.quiz[this.state.activeQuestion].answers}
+              question={this.state.quiz[this.state.activeQuestion].question}
+              onAnswerClick={this.onAnswerClickHandler}
+              quizLength={this.state.quiz.length}
+              answerNumber={this.state.activeQuestion + 1}
+              state={this.state.answerState}
+            />
+          )}
+        </div>
+      </div>
+    );
+  }
 }
 
-export default Quiz
+export default Quiz;
--- react-quiz/src/hoc/Layout/Layout.js
+++ react-quiz/src/hoc/Layout/Layout.js
@@ -1,45 +1,39 @@
-import React, {Component} from 'react'
-import classes from './Layout.module.css'
-import MenuToggle from '../../Components/Navigation/MenuToggle/MenuToggle'
-import Drawer from '../../Components/Navigation/Drawer/Drawer'
+import React, { Component } from "react";
+import classes from "./Layout.module.css";
+import MenuToggle from "../../Components/Navigation/MenuToggle/MenuToggle";
+import Drawer from "../../Components/Navigation/Drawer/Drawer";
 
 class Layout extends Component {
+  state = {
+    menu: false,
+  };
 
-    state = {
-        menu: false
-    }
+  toggleMenuHandler = () => {
+    this.setState({
+      menu: !this.state.menu,
+    });
+  };
 
-    toggleMenuHandler = () => {
-        this.setState({
-            menu: !this.state.menu
-        });
-    }
+  menuCloseHandler = () => {
+    this.setState({
+      menu: false,
+    });
+  };
 
-    menuCloseHandler = () => {
-        this.setState({
-            menu: false
-        });
-    }
+  render() {
+    return (
+      <div className={classes.Layout}>
+        <Drawer isOpen={this.state.menu} onClose={this.menuCloseHandler} />
 
-    render() {
-        return (
-            <div className={classes.Layout}>
-                <Drawer
-                    isOpen={this.state.menu}
-                    onClose={this.menuCloseHandler}
-                />
+        <MenuToggle
+          onToggle={this.toggleMenuHandler}
+          isOpen={this.state.menu}
+        />
 
-                <MenuToggle
-                    onToggle={this.toggleMenuHandler}
-                    isOpen={this.state.menu}
-                />
-
-                <main>
-                    {this.props.children}
-                </main>
-            </div>
-        );
-    }
-};
+        <main>{this.props.children}</main>
+      </div>
+    );
+  }
+}
 
 export default Layout;
