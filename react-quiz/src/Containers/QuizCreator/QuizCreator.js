b08a0462399bf3d13dfc2967143fb52d7a5db212
ivan.ivanov <vanya_ivanivanov_2021_ivanov@mail.ru>
2021-05-02 22:44:57 +0300

    add redux

--- react-quiz/src/Containers/QuizCreator/QuizCreator.js
+++ react-quiz/src/Containers/QuizCreator/QuizCreator.js
@@ -9,7 +9,11 @@ import {
 import Input from "../../Components/UI/Input/Input";
 import Auxiliary from "../../hoc/Auxiliary/Auxiliary";
 import Select from "../../Components/UI/Select/Select";
-import axios from "../../axios/axios-quiz";
+import { connect } from "react-redux";
+import {
+  createQuizQuestion,
+  finishCreateQuiz,
+} from "../../store/actions/create";
 
 function createOptionControl(number) {
   return createControl(
@@ -38,9 +42,8 @@ function createFormControls() {
   };
 }
 
-export default class QuizCreator extends Component {
+class QuizCreator extends Component {
   state = {
-    quiz: [],
     isFormValid: false,
     rightAnswerId: 1,
     formControls: createFormControls(),
@@ -53,9 +56,6 @@ export default class QuizCreator extends Component {
   addQuestionHandler = (event) => {
     event.preventDefault();
 
-    const quiz = this.state.quiz.concat();
-    const index = quiz.length + 1;
-
     const {
       question,
       option1,
@@ -66,7 +66,7 @@ export default class QuizCreator extends Component {
 
     const questionItem = {
       question: question.value,
-      id: index,
+      id: this.props.quiz.length + 1,
       rightAnswerId: this.state.rightAnswerId,
       answers: [
         { text: option1.value, id: option1.id },
@@ -76,30 +76,24 @@ export default class QuizCreator extends Component {
       ],
     };
 
-    quiz.push(questionItem);
+    this.props.createQuizQuestion(questionItem);
 
     this.setState({
-      quiz,
       isFormValid: false,
       rightAnswerId: 1,
       formControls: createFormControls(),
     });
   };
 
-  createQuizHandler = async (event) => {
+  createQuizHandler = (event) => {
     event.preventDefault();
 
-    try {
-      await axios.post("quizes.json", this.state.quiz);
-      this.setState({
-        quiz: [],
-        isFormValid: false,
-        rightAnswerId: 1,
-        formControls: createFormControls(),
-      });
-    } catch (e) {
-      console.log(e);
-    }
+    this.setState({
+      isFormValid: false,
+      rightAnswerId: 1,
+      formControls: createFormControls(),
+    });
+    this.props.finishCreateQuiz();
   };
 
   changeHandler = (value, controlName) => {
@@ -180,7 +174,7 @@ export default class QuizCreator extends Component {
             <Button
               type="success"
               onClick={this.createQuizHandler}
-              disabled={this.state.quiz.length === 0}
+              disabled={this.props.quiz.length === 0}
             >
               Создать тест
             </Button>
@@ -190,3 +184,18 @@ export default class QuizCreator extends Component {
     );
   }
 }
+
+function mapStateToProps(state) {
+  return {
+    quiz: state.create.quiz,
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    createQuizQuestion: (item) => dispatch(createQuizQuestion(item)),
+    finishCreateQuiz: () => dispatch(finishCreateQuiz()),
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(QuizCreator);
--- react-quiz/src/Containers/QuizList/QuizList.js
+++ react-quiz/src/Containers/QuizList/QuizList.js
@@ -1,17 +1,13 @@
 import { Component } from "react";
 import { NavLink } from "react-router-dom";
 import classes from "./QuizList.module.css";
+import { connect } from "react-redux";
 import Loader from "../../Components/UI/Loader/Loader";
-import axios from "../../axios/axios-quiz";
+import { fetchQuizes } from "../../store/actions/quiz";
 
-export default class QuizList extends Component {
-  state = {
-    quizes: [],
-    loading: true,
-  };
-
+class QuizList extends Component {
   renderQuizes() {
-    return this.state.quizes.map((quiz) => {
+    return this.props.quizes.map((quiz) => {
       return (
         <li key={quiz.id}>
           <NavLink to={"/quiz/" + quiz.id}>{quiz.name}</NavLink>
@@ -20,26 +16,8 @@ export default class QuizList extends Component {
     });
   }
 
-  async componentDidMount() {
-    try {
-      const response = await axios.get("quizes.json");
-
-      const quizes = [];
-
-      Object.keys(response.data).forEach((key, index) => {
-        quizes.push({
-          id: key,
-          name: `Тест №${index + 1}`,
-        });
-      });
-
-      this.setState({
-        quizes,
-        loading: false,
-      });
-    } catch (e) {
-      console.log(e);
-    }
+  componentDidMount() {
+    this.props.fetchQuizes();
   }
 
   render() {
@@ -48,9 +26,28 @@ export default class QuizList extends Component {
         <div>
           <h1>Список тестов</h1>
 
-          {this.state.loading ? <Loader /> : <ul>{this.renderQuizes()}</ul>}
+          {this.props.loading && this.props.quizes.length !== 0 ? (
+            <Loader />
+          ) : (
+            <ul>{this.renderQuizes()}</ul>
+          )}
         </div>
       </div>
     );
   }
 }
+
+function mapStateToProps(state) {
+  return {
+    quizes: state.quiz.quizes,
+    loading: state.quiz.loading,
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    fetchQuizes: () => dispatch(fetchQuizes()),
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(QuizList);
